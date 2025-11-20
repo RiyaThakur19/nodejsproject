f08037fc7009e79061d0b812630ca4ddb6639f95
RiyaThakur19 <thakuriya1902@gmail.com>
2025-11-20 12:59:08 +0530

    Update authServices.js

--- Admin/src/features/auth/authServices.js
+++ Admin/src/features/auth/authServices.js
@@ -2,10 +2,6 @@ import axios from "axios";
 import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/baseUrl";
 
-const getTokenFromLocalStorage = localStorage.get("user")
-  ? JSON.parse(localStorage.getItem("user"))
-  : null;
-
 const login = async (user) => {
   const response = await axios.post(`${base_url}user/admin-login`, user);
   if (response.data) {
